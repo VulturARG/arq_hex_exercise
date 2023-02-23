55381a68b880b2efbb4396a6c8382a533e674c30
VulturARG <luisbriones@gmail.com>
2023-02-23 10:49:52 -0300

    First commit

--- seudo_django/views.py
+++ seudo_django/views.py
@@ -1,8 +1,5 @@
-from adapter.logic.package.package_logic import PackageLogic
-
-
 class PackageViewSet:
     """Is good to be a viewsets.ViewSet."""
 
     def some_action(self):
-        return PackageLogic.get_volumes()
+        pass
